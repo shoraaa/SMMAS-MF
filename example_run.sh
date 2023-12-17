b400719e5c11806e713478c2c1f0a2949a94433d
shoraaa <shoraaa@protonmail.com>
2023-12-18 05:37:15 +0700

    k = n

--- example_run.sh
+++ example_run.sh
@@ -7,4 +7,4 @@
 # ITERATIONS: 1000    (-n 1000)
 # TRIES: 1            (-r 1)
 
-./acotsp-AVX2 -i instances/pr2392.tsp -m 80 -k 1024 -l 1 -n 1000 -r 10
+./acotsp-AVX2 -i instances/pr2392.tsp -m 80 -k 1024 -l 0 -n 1000 -r 10
